dda32e6e4dade975c0e5703503c9bd63c0645e2b
Jim Martens <github@2martens.de>
2025-12-02 17:20:42 +0100

    change: Only perform string concatenation if it is required
    style: Apply formatting

--- Sources/AdventOfCode2025/day2.swift
+++ Sources/AdventOfCode2025/day2.swift
@@ -24,16 +24,22 @@ import Foundation
 func day2(_ baseDirectory: URL) throws {
     let filePath = baseDirectory.appending(path: "inputs/day2.txt")
     let instructions = try String(contentsOf: filePath, encoding: .utf8)
-    
+
     let startPart1 = Date()
-    let sumPart1 = calculateSumOfInvalidIDs(instructions, filter: isInvalidPart1)
+    let sumPart1 = calculateSumOfInvalidIDs(
+        instructions,
+        filter: isInvalidPart1
+    )
     let endPart1 = Date()
     let durationPart1: TimeInterval = endPart1.timeIntervalSince(startPart1)
     print("The sum of invalid IDs part 1: \(sumPart1)")
     print("Time taken part 1: \(durationPart1) seconds")
-    
+
     let startPart2 = Date()
-    let sumPart2 = calculateSumOfInvalidIDs(instructions, filter: isInvalidPart2)
+    let sumPart2 = calculateSumOfInvalidIDs(
+        instructions,
+        filter: isInvalidPart2
+    )
     let endPart2 = Date()
     let durationPart2: TimeInterval = endPart2.timeIntervalSince(startPart2)
     print("The sum of invalid IDs part 2: \(sumPart2)")
@@ -45,17 +51,17 @@ struct ProductIDRange {
     let end: Int
     let invalidFilter: (Int) -> Bool
     var invalidIDs: [Int] = []
-    
+
     /**
      Returns number of invalid IDs
-            
+    
      - Returns: count of invalid IDs as identified by `invalidFilter`
      - Precondition: You need to call `identifyInvalidIDs` first
      */
     func getNumberOfInvalidIDs() -> Int {
         invalidIDs.count
     }
-    
+
     mutating func identifyInvalidIDs() {
         let range = start...end
         let invalidIDs = range.filter(invalidFilter)
@@ -65,10 +71,13 @@ struct ProductIDRange {
 
 func isInvalidPart1(_ id: Int) -> Bool {
     let idAsString = String(id)
-    let middleIndex = idAsString.index(idAsString.startIndex, offsetBy: idAsString.count / 2)
+    let middleIndex = idAsString.index(
+        idAsString.startIndex,
+        offsetBy: idAsString.count / 2
+    )
     let firstHalf = idAsString[..<middleIndex]
     let secondHalf = idAsString[middleIndex...]
-    
+
     return firstHalf == secondHalf
 }
 
@@ -76,13 +85,18 @@ func isInvalidPart2(_ id: Int) -> Bool {
     let idAsString = String(id)
     var dividers: [Int] = []
     for multiplier in 1...idAsString.count {
-        multiplier > 1 && idAsString.count.isMultiple(of: multiplier) ? dividers.append(multiplier) : ()
+        multiplier > 1 && idAsString.count.isMultiple(of: multiplier)
+            ? dividers.append(multiplier) : ()
     }
-    
+
     for offset in 1...Int(ceil(Double(idAsString.count) / 2.0)) {
         let index = idAsString.index(idAsString.startIndex, offsetBy: offset)
         let segment = idAsString[..<index]
         for divider in dividers {
+            if !idAsString.hasSuffix(segment) {
+                continue
+            }
+
             var multipliedString: String = ""
             for _ in 0..<divider {
                 multipliedString += segment.description
@@ -92,24 +106,32 @@ func isInvalidPart2(_ id: Int) -> Bool {
             }
         }
     }
-    
+
     return false
 }
 
-func calculateSumOfInvalidIDs(_ instructions: String, filter: @escaping (Int) -> Bool) -> Int {
+func calculateSumOfInvalidIDs(
+    _ instructions: String,
+    filter: @escaping (Int) -> Bool
+) -> Int {
     var invalidIDs: [Int] = []
-    
-    instructions.trimmingCharacters(in: ["\n"]).split(separator: ",").forEach { instruction in
+
+    instructions.trimmingCharacters(in: ["\n"]).split(separator: ",").forEach {
+        instruction in
         let parts = instruction.split(separator: "-")
         let start = Int(parts[0])!
         let end = Int(parts[1])!
-        
-        var range = ProductIDRange(start: start, end: end, invalidFilter: filter)
+
+        var range = ProductIDRange(
+            start: start,
+            end: end,
+            invalidFilter: filter
+        )
         range.identifyInvalidIDs()
         invalidIDs.append(contentsOf: range.invalidIDs)
     }
-    
+
     let sum = Set(invalidIDs).reduce(0, +)
-    
+
     return sum
 }
